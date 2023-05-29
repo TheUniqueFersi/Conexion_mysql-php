9d847f9760998057dc7ddbfdb1d234d162c1cca8
TheUniqueFersi <holafersa@gmail.com>
2023-05-29 06:10:12 -0600

    feat: añade archivos raiz principales

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conexión mysql - php</title>
+</head>
+<body>
+    <form action="./" method="post">
+        <fieldset>
+            <label for=""></label>
+            <input type="text">
+
+            <label for=""></label>
+            <input type="text">
+
+            <label for=""></label>
+            <input type="text">
+
+
+            <button type="submit">Enviar</button>
+            <button type="reset">Reset</button>
+        </fieldset>
+    </form>    
+</body>
+</html>
---
+++ myTuiter.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tuiter</title>
+    <link rel="shortcut icon" href="./statics/media/img/resources/ig.png" type="image/x-icon">
+    <link rel="stylesheet" href="./statics/styles/index.css">
+</head>
+<body>
+    <nav id="contenedor-navbar">
+        <div>
+            <?php
+                $ruta_foto = "./statics/media/img/perfil_fersa.png";
+                echo "<img src='$ruta_foto'' alt='imagen de $usuario'>";
+            ?>
+        </div>
+        
+        
+    </nav>
+    <?php
+
+    ?>
+</body>
+</html>
