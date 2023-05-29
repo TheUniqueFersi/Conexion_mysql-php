41fe5a69d6680a61158aa8b13c24f121b18a3ed8
TheUniqueFersi <holafersa@gmail.com>
2023-05-28 22:01:03 -0600

    feat: añade carpetas principales

---
+++ docs/notes.php
@@ -0,0 +1,24 @@
+<?php
+    //ver conexion con mysql (CONSTANTES)
+    const DBHOST = "localhost";
+    const DBUSER = "";
+    const PASSWORD = "";
+    const 
+
+
+
+    /*
+
+    Usuarios ()
+    Registren usuarios,
+    que verifiquen si existe usuario.
+
+    
+    Miercoles 11:59
+    
+
+
+    */
+
+
+?>
