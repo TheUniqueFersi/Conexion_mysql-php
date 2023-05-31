1b842c21813e74c502bd6b05a3ae01850826853f
TheUniqueFersi <holafersa@gmail.com>
2023-05-31 10:40:31 -0600

    fix: corrige el favicon

--- myTuiter.php
+++ myTuiter.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tuiter</title>
-    <link rel="shortcut icon" href="./statics/media/img/resources/tuiter.png" type="image/x-icon">
+    <link rel="icon" href="./statics/media/img/resources/tuiter.png" type="image/png">
     <link rel="stylesheet" href="./statics/styles/index.css">
 </head>
 <body>
